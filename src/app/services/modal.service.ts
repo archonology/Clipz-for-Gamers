020bbcab2da9d5f795e98d8a6d82ffa13e1349d0
Reed Meher <reed.meher@gmail.com>
2024-03-07 14:49:47 -0600

    toggle modal visibility development

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  // visible object is private to prevent other components from accessing it.
+  public visible = false;
+
+  constructor() {}
+
+  isModalOpen() {
+    return this.visible;
+  }
+
+  toggleModal() {
+    this.visible = !this.visible
+  }
+}
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { AuthModalComponent } from '../../user/auth-modal/auth-modal.component';
+import { ModalService } from '../../services/modal.service';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [AuthModalComponent],
+  imports: [AuthModalComponent, NgClass],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
+  constructor(public modal: ModalService) {
 
+  }
 }
