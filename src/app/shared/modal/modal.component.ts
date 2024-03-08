e7d93b2ec6f7277f951b443e8b677e547c5886cd
Reed Meher <reed.meher@gmail.com>
2024-03-08 12:57:36 -0600

    ensuring modal implements native element

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef } from '@angular/core';
 import { AuthModalComponent } from '../../user/auth-modal/auth-modal.component';
 import { ModalService } from '../../services/modal.service';
 import { NgClass } from '@angular/common';
@@ -11,8 +11,15 @@ import { NgClass } from '@angular/common';
   styleUrl: './modal.component.css',
 })
 export class ModalComponent {
-  @Input() modalID = ''
-  constructor(public modal: ModalService) {}
+  @Input() modalID = '';
+
+  constructor(public modal: ModalService, public el: ElementRef) {
+
+  }
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
+  }
+
   closeModal() {
     this.modal.toggleModal(this.modalID);
   }
