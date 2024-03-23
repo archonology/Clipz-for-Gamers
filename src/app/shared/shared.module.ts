0ecc40e0b5fe759715bbb46087d7cd3d97171349
Reed Meher <reed.meher@gmail.com>
2024-03-23 11:25:15 -0500

    building tabs for login/registration

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
-
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    ModalComponent,
-  ],
-  exports: [
-    ModalComponent
-  ]
+  imports: [CommonModule, ModalComponent, TabsContainerComponent, TabComponent],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
