cd5a37a645a71ade85b590f532a3e7701a54a7ca
Reed Meher <reed.meher@gmail.com>
2024-03-23 11:47:12 -0500

    dynamic tab components rendered

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   standalone: true,
   imports: [],
   templateUrl: './tab.component.html',
-  styleUrl: './tab.component.css'
+  styleUrl: './tab.component.css',
 })
 export class TabComponent {
-
+  @Input() tabTitle = '';
 }
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+import { CommonModule, NgFor } from '@angular/common';
 @Component({
   selector: 'app-tabs-container',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './tabs-container.component.html',
-  styleUrl: './tabs-container.component.css'
+  styleUrl: './tabs-container.component.css',
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
+  ngAfterContentInit(): void {
+  
+    console.log(this.tabs);
+  }
 }
