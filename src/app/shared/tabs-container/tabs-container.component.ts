c093c6b7a2c9d63c8e2b52d05db82a2cb51ee9ce
Reed Meher <reed.meher@gmail.com>
2024-03-26 15:32:48 -0500

    active tabs click event

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,9 @@
-import { Component, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  AfterContentInit,
+  QueryList,
+} from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 import { CommonModule, NgFor } from '@angular/common';
 @Component({
@@ -9,10 +14,20 @@ import { CommonModule, NgFor } from '@angular/common';
   styleUrl: './tabs-container.component.css',
 })
 export class TabsContainerComponent implements AfterContentInit {
-  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
+    new QueryList();
 
   ngAfterContentInit(): void {
-  
-    console.log(this.tabs);
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((tab) => {
+      tab.active = false;
+    });
+
+    tab.active = true;
   }
 }
