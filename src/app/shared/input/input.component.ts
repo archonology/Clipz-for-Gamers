ca053bd2e2a2a1f344c21ac90cfa2e7b7efbc39a
Reed Meher <reed@meherdevs.com>
2024-05-13 15:21:59 -0500

    prep for input masking

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -2,11 +2,14 @@ import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
+// ngx-mask will be used for masking the phone number field.
+import { NgxMaskDirective } from 'ngx-mask';
+
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, NgxMaskDirective],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
@@ -14,4 +17,5 @@ export class InputComponent {
   @Input() control: FormControl = new FormControl()
   @Input() type = 'text'
   @Input() placeholder = ''
+  @Input() format = ''
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { TabsContainerComponent } from './tabs-container/tabs-container.componen
 import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { provideEnvironmentNgxMask, NgxMaskDirective } from 'ngx-mask';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
+  imports: [CommonModule, ModalComponent, TabsContainerComponent, TabComponent, InputComponent, NgxMaskDirective],
   exports: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent, ReactiveFormsModule],
+  providers: [provideEnvironmentNgxMask()],
 })
 export class SharedModule {}
