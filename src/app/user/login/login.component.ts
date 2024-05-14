87c0ad12753f84f7b35d538226fce74d9d74f62f
Reed Meher <reed@meherdevs.com>
2024-05-14 10:55:50 -0500

    status alert and login function added

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
+import { AlertComponent } from '../../shared/alert/alert.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, AlertComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -14,4 +15,15 @@ export class LoginComponent {
     email: '',
     password: ''
   }
+
+  showAlert = false
+  alertMsg = 'Please wait! You are being logged in...'
+  alertColor = 'blue'
+
+  login() {
+    this.showAlert = true
+    this.alertMsg = 'Please wait! You are being logged in...'
+    this.alertColor = 'blue'
+  }
+
 }
