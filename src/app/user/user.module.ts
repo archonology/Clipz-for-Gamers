349ef853ffa8c75fdffcd963503adff3c62bfe94
Reed Meher <reed@meherdevs.com>
2024-05-14 10:08:28 -0500

    building template form for login

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-// import { ReactiveFormsModule } from '@angular/forms';
+
 import { SharedModule } from '../shared/shared.module';
 import { RegistrationComponent } from './registration/registration.component';
 
