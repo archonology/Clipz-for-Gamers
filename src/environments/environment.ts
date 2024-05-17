361ce814472eeae97873307f89fe66de8fa12e40
Reed Meher <reed@meherdevs.com>
2024-05-17 12:33:10 -0500

    connecting firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyD7riexlUGy8K5-CukPBAsfcZ1xfI5QZcA",
+        authDomain: "clips-f98f4.firebaseapp.com",
+        projectId: "clips-f98f4",
+        storageBucket: "clips-f98f4.appspot.com",
+        messagingSenderId: "994423968230",
+        appId: "1:994423968230:web:3f1cd7f056c49bf63ac627",
+        measurementId: "G-NDS1JG72ZV"
+    }
+};
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+// a safe list is added so we can dynamically change the tailwind. Tailwind will ignore the dynamic color changes by default.
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
+  safelist: ['bg-blue-400', 'bg-green-400', 'bg-red-400'],
   theme: {
     fontFamily: {
       'sans': ['Roboto', 'Arial', 'sans-serif'],
