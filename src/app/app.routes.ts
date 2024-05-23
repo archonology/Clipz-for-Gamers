7c959c5d3dad1e5dd8062afd99f4cf3cd59afe4b
Reed Meher <reed@meherdevs.com>
2024-05-23 10:36:53 -0500

    404 page for wildcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { ManageComponent } from './video/manage/manage.component';
 import { UploadComponent } from './video/upload/upload.component';
 import { ClipComponent } from './clip/clip.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 // Angular 14+ you don't need RouterModules to handle child routes -- just use loadComponent here to reduce complexity and keep the lazy load benefits. Also, it says in the docs the RouterModule is still available, but with the new standalone first module structure, there are complications that I've yet to understand how to resolve.
 export const routes: Routes = [
     {
@@ -31,5 +32,9 @@ export const routes: Routes = [
     {
         path: 'clip/:id',
         component: ClipComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     },
 ];
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -22,6 +22,17 @@ export class ManageComponent {
     const { value } = (event.target as HTMLSelectElement)
 
     this.router.navigateByUrl(`/manage?sort=${value}`)
+
+    // more nuanced but more powerful way to guide navigation -- has same behavior as above with these settings.
+    //   this.router.navigate([], {
+    //     relativeTo: this.route,
+    //     queryParams: {
+    //       sort: value
+    //     }
+    //   })
   }
 
 }
+
+// Path Parameters: to be used for returning a single resource or multiple resources.
+// Query Parameters: to be used for sorting/filtering through data
