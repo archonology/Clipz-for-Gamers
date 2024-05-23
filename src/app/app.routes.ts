fe0f384c554840d73ed7bd16ddbfdb6e2bea3bb3
Reed Meher <reed@meherdevs.com>
2024-05-23 11:12:22 -0500

    workaround for defunt AngularAuthGuard via canActive method

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,12 @@ import { ManageComponent } from './video/manage/manage.component';
 import { UploadComponent } from './video/upload/upload.component';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
 // Angular 14+ you don't need RouterModules to handle child routes -- just use loadComponent here to reduce complexity and keep the lazy load benefits. Also, it says in the docs the RouterModule is still available, but with the new standalone first module structure, there are complications that I've yet to understand how to resolve.
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/']);
+
 export const routes: Routes = [
     {
         path: '',
@@ -15,9 +20,11 @@ export const routes: Routes = [
         path: 'about',
         component: AboutComponent
     },
+    // canActive needs to come before the data because the data object interferes with canActivate if it fires first -- because it is redirecting the page before canActivate can complete, I believe.
     {
         path: 'manage',
         loadComponent: () => ManageComponent,
+        ...canActivate(redirectUnauthorizedToLogin),
         data: {
             authOnly: true
         }
