96f764a9c1903cd121a79aca4b1d9129875778ed
Reed Meher <reed@meherdevs.com>
2024-05-23 10:16:46 -0500

    setting query params based on user choices

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
@@ -9,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './manage.component.css'
 })
 export class ManageComponent {
+  videoOrder = '1'
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params['sort'] === '2' ? params['sort'] : 1;
+    })
+  }
+
+  // Elements can be set as types in TS
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement)
+
+    this.router.navigateByUrl(`/manage?sort=${value}`)
+  }
 
 }
