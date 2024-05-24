1464f1ff36c2a09a1d3bfb38028fa4792de9e887
Reed Meher <reed@meherdevs.com>
2024-05-24 13:11:32 -0500

    clip submission handling

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { EventBlockerDirective } from '../../shared/directives/event-blocker.directive';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [EventBlockerDirective, NgClass],
+  imports: [EventBlockerDirective, NgClass, NgIf],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.css'
 })
 export class UploadComponent {
   //set a custom hover event to keep the visuals for the user
   isDragover = false
-
+  nextStep = false
+  file: File | null = null
   storeFile($event: Event) {
     this.isDragover = false
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+    this.nextStep = true
   }
 }
