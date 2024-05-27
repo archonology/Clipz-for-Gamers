86de02588991913f0bbd051981124dda1f6cc132
Reed Meher <reed@meherdevs.com>
2024-05-27 12:36:57 -0500

    link to upload via manage page

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Router, RouterLink, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css'
 })
