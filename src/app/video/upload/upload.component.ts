9243cd706d90e7acf3e63536e568779d03919048
Reed Meher <reed@meherdevs.com>
2024-05-28 12:15:05 -0500

    corrected: from uid to fileName on redirect

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -108,7 +108,7 @@ export class UploadComponent implements OnDestroy {
         }
         const clipDocRef = await this.clipService.createClip(clip)
         // because of recent AngularFire compatability issues, I am doing the query of the just created clip here (with getClip), rather than the createClip function, though it isn't ideal. I ran into issues with setDoc method not returning the kind of promise I needed, and the new AngularFire tree shaking method doesn't have any supporting docs at the time of creation. Additionally, the modular way was goving me a lot of trouble when dealing with services and Angular's new 'standalone' build defaults.
-        const getClip = doc(this.db, 'clips', clipDocRef.uid)
+        const getClip = doc(this.db, 'clips', clipDocRef.fileName)
 
         this.alertColor = 'green'
         this.alertMsg = 'Success! Your clip is ready to share with the world!'
