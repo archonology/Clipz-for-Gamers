8a94ba83a45af28503076ea9298a4e22eb2734b6
Reed Meher <reed@meherdevs.com>
2024-05-30 16:08:07 -0500

    datePipe implemented for homepage

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -3,13 +3,16 @@ import { OnInit } from '@angular/core';
 import { ClipService } from '../services/clip.service';
 import { NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { FbTimestampPipe } from '../pipes/fb-timestamp.pipe';
 
 @Component({
   selector: 'app-clips-list',
   standalone: true,
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink, FbTimestampPipe],
   templateUrl: './clips-list.component.html',
-  styleUrl: './clips-list.component.css'
+  styleUrl: './clips-list.component.css',
+  providers: [DatePipe]
 })
 export class ClipsListComponent implements OnInit {
 
