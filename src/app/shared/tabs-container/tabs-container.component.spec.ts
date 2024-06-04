765ef7313bfbe9b43e7cc8c23824a826245c11ed
Reed Meher <reed@meherdevs.com>
2024-06-04 09:33:50 -0500

    tabs length test

--- src/app/shared/tabs-container/tabs-container.component.spec.ts
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -2,13 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { TabsContainerComponent } from './tabs-container.component';
 import { TabComponent } from '../tab/tab.component';
+import { By } from '@angular/platform-browser';
 
 @Component({
   template: `
-  <app-tabs-container>
-    <app-tab tabTitle='Tab 1'>Tab 1</app-tab>
-    <app-tab tabTitle='Tab 3'>Tab 2</app-tab>
-  </app-tabs-container>
+    <app-tabs-container>
+      <app-tab tabTitle='Tab 1'>Tab 1</app-tab>
+      <app-tab tabTitle='Tab 3'>Tab 2</app-tab>
+    </app-tabs-container>
   `
 })
 class TestHostComponent { }
@@ -31,4 +32,17 @@ describe('TabsContainerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have two tabs', () => {
+    // select the element method
+    const tabs = fixture.debugElement.queryAll(By.css('li'));
+    // select the component method
+    const containerComponent = fixture.debugElement.query(By.directive(TabsContainerComponent))
+
+    const tabsProp = containerComponent.componentInstance.tabs;
+
+    expect(tabs.length).toBe(2);
+    expect(tabsProp.length).toBe(2);
+  })
+
 });
