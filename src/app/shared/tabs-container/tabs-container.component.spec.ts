398f3fdeb4c643fdc19efbdac21d565ee61ab27d
Reed Meher <reed@meherdevs.com>
2024-06-04 09:26:49 -0500

    testing nested components - tabs

---
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TabsContainerComponent } from './tabs-container.component';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  template: `
+  <app-tabs-container>
+    <app-tab tabTitle='Tab 1'>Tab 1</app-tab>
+    <app-tab tabTitle='Tab 3'>Tab 2</app-tab>
+  </app-tabs-container>
+  `
+})
+class TestHostComponent { }
+describe('TabsContainerComponent', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TabsContainerComponent, TabComponent],
+      declarations: [TestHostComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
